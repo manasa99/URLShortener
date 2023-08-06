10ab7bfabcc9d33ddc7d78574f5c6ec2e70eaf87
Srinivas Vaddi <vaddisrinivas170497@gmail.com>
2023-08-06 18:30:58 -0400

    Updates with full application

---
+++ lambdas/pyredis.py
@@ -0,0 +1,56 @@
+import logging
+import time
+import redis
+import base64
+import os
+
+
+def generate_short_url():
+    timestamp = int(time.time())
+    unique_value = base64.urlsafe_b64encode(str(timestamp).encode()).decode().rstrip("=")
+    return unique_value
+
+
+def set_url(redis_client, short_url=None, original_url=None, ex=86400):
+    try:
+        if short_url is None:
+            short_url = generate_short_url()
+        redis_client.set(short_url, original_url, ex=ex)
+        logging.info(f'Generated shortened URL: {short_url}')
+        return short_url
+    except redis.RedisError as e:
+        logging.exception(f'Error setting url: {short_url}. Error: {e}')
+
+
+def get_url(redis_client, short_url):
+    try:
+        original_url = redis_client.get(short_url)
+        exp = redis_client.ttl(short_url)
+        if original_url is not None:
+            return original_url, exp
+        else:
+            return None
+    except redis.RedisError as e:
+        logging.exception(f'Error getting url: {short_url}. Error: {e}')
+        return None
+
+
+def main(event, context):
+    print("in main")
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    print(event)
+    print(context)
+    host = os.environ.get('REDIS_HOST', 'localhost')
+    port = int(os.environ.get('REDIS_PORT', 6379))
+    password = os.environ.get('REDIS_AUTH', 'redispass')
+    redis_client = redis.StrictRedis(host=host, port=port, decode_responses=True, password=password, ssl=True)
+    print(redis_client.info())
+    if event["operation"] == "set":
+        original_url, ex = event["original_url"], event["ex"]
+        short_url = set_url(redis_client=redis_client, original_url=original_url, ex=ex)
+        return short_url
+    elif event["operation"] == "get":
+        short_url = event["short_url"]
+        original_url, expiry = get_url(redis_client=redis_client, short_url=short_url)
+        return original_url, expiry
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import IconButton from "@mui/material/IconButton";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import MediaCard from './Cards'
+import  { useState, useEffect } from "react";
 
-function App() {
+const ThemePaletteModeContext = React.createContext({
+  toggleThemePaletteMode: () => {}
+});
+
+const ToggleThemePaletteMode = () => {
+
+  // useEffect(() => {
+  //   switch (page) {
+  //     case "home":
+  //       loadHome();
+  //       break;
+  //     case "about":
+  //       loadAbout();
+  //       break;
+  //     case "urlshortener":
+  //       loadUrlShortener();
+  //       break;
+  //     case "pastebin":
+  //       loadPastebin();
+  //       break;
+  //     default:
+  //       loadHome();
+  //       break;
+  //   }
+  // }, [page]);
+
+  const theme = useTheme();
+  const themePaletteModeContext = React.useContext(ThemePaletteModeContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "left",
+        bgcolor: "background.default",
+        color: "text.primary"
+      }}
+    >
+      <IconButton
+        sx={{ ml: 1 }}
+        onClick={themePaletteModeContext.toggleThemePaletteMode}
+        color="inherit"
+      >
+        {theme.palette.mode === "dark" ? (
+          <Brightness7Icon />
+        ) : (
+          <Brightness4Icon />
+        )}
+      </IconButton>
+    </Box>
+  );
+};
+
+export default function App() {
+  const [page, setPage] = useState("home"); 
+
+  const isSystemDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [themePaletteMode, setThemePaletteMode] = React.useState(
+    isSystemDarkMode ? "dark" : "light"
+  );
+
+  const themePaletteModeContextProvider = React.useMemo(
+    () => ({
+      toggleThemePaletteMode: () => {
+        setThemePaletteMode((prevMode) =>
+          prevMode === "light" ? "dark" : "light"
+        );
+      }
+    }),
+    []
+  );
+  
+  const props_arr = {url:"https://public-apps-thetechcruise.s3.us-east-2.amazonaws.com/public/document.jpeg",title:"URL Shortener",description:"Oh my such good apples pie"}
+  const themeProvider = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: themePaletteMode
+        }
+      }),
+    [themePaletteMode]
+  );
+
+  
+  return (
+    <ThemePaletteModeContext.Provider value={themePaletteModeContextProvider}>
+      <ThemeProvider theme={themeProvider}>
+      <AppBar  position="sticky" component='nav'>
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon >
+
+            </MenuIcon>
+          </IconButton>
+          <Typography variant="h4" color="inherit" component="div">
+            Apps from TheTechCruise
+          </Typography>
+        </Toolbar>
+      </AppBar>
+        <ToggleThemePaletteMode />
+        {page === "home" && <MediaCard props = {props_arr}></MediaCard>}
+        {page === "urlshortener" && <URLShortener />}
+
+        Hello World
+      </ThemeProvider>
+    </ThemePaletteModeContext.Provider>
   );
 }
-
-export default App;
